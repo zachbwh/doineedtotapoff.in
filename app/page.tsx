f8e988cf8498e50fb9c30f31a5805f03a227bc99
zachbwh <zachbwh@gmail.com>
2024-02-18 18:13:20 +1100

    basic redirecting on landing page submit

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,29 @@
+"use client";
+
 import H1 from "@/components/ui/h1";
 import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  const submitHandler: (formData: FormData) => void = (formData) => {
+    router.push(`locations/${formData.get("location")}`);
+  };
+
   return (
     <div className="m-auto max-w-4xl items-center h-full">
-      <H1 className="block md:inline pb-2">Do I need to tap off in</H1>
-      <H1 className="block md:inline">
-        <Input
-          className="!text-4xl !font-extrabold !tracking-tight lg:!text-5xl max-w-[250px] lg:!max-w-[350px] mr-2 md:mx-2 px-2 inline"
-          placeholder="Melbourne"
-        />
-        ?
-      </H1>
+      <form action={submitHandler}>
+        <H1 className="block md:inline pb-2">Do I need to tap off in</H1>
+        <H1 className="block md:inline">
+          <Input
+            className="!text-4xl !font-extrabold !tracking-tight lg:!text-5xl max-w-[250px] lg:!max-w-[350px] mr-2 md:mx-2 px-2 inline"
+            placeholder="Melbourne"
+            name="location"
+          />
+          ?
+        </H1>
+      </form>
     </div>
   );
 }
