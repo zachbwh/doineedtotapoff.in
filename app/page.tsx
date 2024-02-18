2b6072846bc66620aeb08b080b6ee6d411c9b0b5
zachbwh <zachbwh@gmail.com>
2024-02-18 18:03:09 +1100

    improve mobile styling on landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,15 @@ import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div className="m-auto max-w-4xl flex items-center h-full">
-      <H1>Do I need to tap off in </H1>
-      <Input
-        className="!text-4xl !font-extrabold !tracking-tight lg:!text-5xl max-w-[250px] lg:!max-w-[350px] mx-2 px-2"
-        placeholder="Melbourne"
-      />
-      <H1>?</H1>
+    <div className="m-auto max-w-4xl items-center h-full">
+      <H1 className="block md:inline pb-2">Do I need to tap off in</H1>
+      <H1 className="block md:inline">
+        <Input
+          className="!text-4xl !font-extrabold !tracking-tight lg:!text-5xl max-w-[250px] lg:!max-w-[350px] mr-2 md:mx-2 px-2 inline"
+          placeholder="Melbourne"
+        />
+        ?
+      </H1>
     </div>
   );
 }
