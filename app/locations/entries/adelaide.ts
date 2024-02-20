3c1d6cbdb6f63c20912973689b79bcafe2603047
zachbwh <zachbwh@gmail.com>
2024-02-20 20:59:05 +1100

    add adelaide

---
+++ app/locations/entries/adelaide.ts
@@ -0,0 +1,11 @@
+import { LocationEntry } from "./LocationEntry";
+
+export const AdelaideEntry: LocationEntry = {
+  names: ["Adelaide"],
+  country: "Australia",
+  tapOffRequired: "NO",
+  rules: [],
+  source: "https://www.adelaidemetro.com.au/plan-a-trip/visiting-adelaide",
+  lastChecked:
+    "Sun Feb 20 2024 20:58:52 GMT+1100 (Australian Eastern Daylight Time)",
+};
--- app/locations/entries/index.ts
+++ app/locations/entries/index.ts
@@ -1,9 +1,15 @@
 import { LocationEntry } from "./LocationEntry";
+import { AdelaideEntry } from "./adelaide";
 import { AucklandEntry } from "./auckland";
 import { MelbourneEntry } from "./melbourne";
 import { SydneyEntry } from "./sydney";
 
-const locationEntries = [AucklandEntry, MelbourneEntry, SydneyEntry];
+const locationEntries = [
+  AucklandEntry,
+  MelbourneEntry,
+  SydneyEntry,
+  AdelaideEntry,
+];
 const locationEntryMap: Record<string, LocationEntry> = {};
 // duplicate locations for each name variant so alternative names have their own paths too
 locationEntries.forEach((locationEntry) => {
