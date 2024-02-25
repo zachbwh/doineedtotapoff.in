815bc947a60a68c8ef51c3acf4ef4898a3945495
zachbwh <zachbwh@gmail.com>
2024-02-25 21:23:37 +1100

    generate basic sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,29 @@
+import { MetadataRoute } from "next";
+import locationEntryMap from "./locations/entries";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://doineedtotapoff.in",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    ...Object.keys(locationEntryMap).map((locationName) => {
+      return {
+        url: `https://doineedtotapoff.in/locations/${encodeURIComponent(
+          locationName
+        )}`,
+        lastModified: new Date(locationEntryMap[locationName].lastChecked),
+        changeFrequency: "weekly",
+        priority: 0.95,
+      } as MetadataRoute.Sitemap[0];
+    }),
+    {
+      url: "https://doineedtotapoff.in/locations",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.1,
+    },
+  ];
+}
